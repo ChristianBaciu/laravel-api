dc864af048dbe6bb1ecf6fd6f265e07f3073de1a
ChristianBaciu <christian3110@outlook.it>
2024-04-08 16:23:00 +0200

    chiamata API aggioranta con le relazioni 'one to many' e 'many to many'

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,12 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projets = Project::all();
+        // $projets = Project::all(); //tutti record
+
+        // 'type' relazione one to many
+        // 'technologies' relazione many to many
+        $projets = Project::with('type', 'technologies')->get(); // per prendere tutti i record anche nelle relazioni
+
 
         return response()->json([
             // success serve per vedere se la chiamata è andata a buon fine/non è obbligatorio metterlo
